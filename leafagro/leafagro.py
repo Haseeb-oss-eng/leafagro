f45b330d98852cfd71f57400c398b43232c4c57e
Haseeb-oss-eng <ibnhabeebulla@gmaol.com>
2024-09-17 19:16:53 +0530

    info added

--- leafagro/leafagro.py
+++ leafagro/leafagro.py
@@ -1,18 +1,25 @@
 """Main module."""
+
 import ipyleaflet
-from ipyleaflet import basemap
+from ipyleaflet import basemaps
 
 class Map(ipyleaflet.Map):
-    def __init__(self, center=[20, 0], zoom=2, **kwargs):
-        super().__init__(center=center, zoom=zoom, **kwargs)
 
-    def add_tile_layer(self,name,url,**kwargs):
+    def __init__(self,center=[20,0],zoom=2,**kwargs):
+        super().__init__(center=center,zoom=zoom,**kwargs)
+        
+        
+    def add_layer_tile(self,url,name,**kwargs):
         layer = ipyleaflet.TileLayer(url=url,name=name,**kwargs)
         self.add(layer)
 
     def add_basemap(self, name):
+
         if isinstance(name,str):
-            url = eval(f"basemap.{name}").build_url()
-            self.add_tile_layer(url,name)
+            url = eval(f"basemaps.{name}").build_url()
+            self.add_layer_tile(url,name)
         else:
             self.add(name)
+
+    def add_layer_control(self,position='topright'):
+        self.add_control(ipyleaflet.LayersControl(position=position))
