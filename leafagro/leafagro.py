ba1903b9a4497e9bc56ec0bba7a0b03bfe882912
Haseeb-oss-eng <ibnhabeebulla@gmaol.com>
2024-09-17 15:51:35 +0530

    Added leafagro syntax

--- leafagro/leafagro.py
+++ leafagro/leafagro.py
@@ -1,1 +1,18 @@
 """Main module."""
+import ipyleaflet
+from ipyleaflet import basemap
+
+class Map(ipyleaflet.Map):
+    def __init__(self, center=[20, 0], zoom=2, **kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
+
+    def add_tile_layer(self,name,url,**kwargs):
+        layer = ipyleaflet.TileLayer(url=url,name=name,**kwargs)
+        self.add(layer)
+
+    def add_basemap(self, name):
+        if isinstance(name,str):
+            url = eval(f"basemap.{name}").build_url()
+            self.add_tile_layer(url,name)
+        else:
+            self.add(name)
